9001c7819bf0d37c254e1bc3041e219d1b94072d
Saccilotto <sacilotto.andre@gmail.com>
2024-03-18 19:17:27 -0300

    fixing karatsuba by hand

--- karatsuba_triplo.cpp
+++ karatsuba_triplo.cpp
@@ -50,11 +50,11 @@ string subtractStrings(string fator1, string fator2) {
 string karatsuba(string numero1, string numero2) {
     int n = numero1.size();
     int m = numero2.size();
-
+    /*
     if (n == 0 || m == 0) {
         return "0";
     }
-
+    */
     // Condição de parada para números pequenos
     if (n == 1 || m == 1) {
         return to_string((numero1[0] - '0') * (numero2[0] - '0'));
@@ -71,17 +71,21 @@ string karatsuba(string numero1, string numero2) {
     }
 
     // Calcula a posição de separação
-    int quebra_indice = n / 3;
+    int quebra_indice_fact1 = n / 3;
+    int quebra_indice_fact2 = m / 3;
 
-    string numero1_esquerda = numero1.substr(0, quebra_indice);
-    string numero1_meio = numero1.substr(quebra_indice, quebra_indice);
-    string numero1_direita = numero1.substr(quebra_indice * 2);
+    // quebra em 3 cada fator da multiplicação
+    string numero1_esquerda = numero1.substr(0, quebra_indice_fact1);
+    string numero1_meio = numero1.substr(quebra_indice_fact1, quebra_indice_fact1);
+    string numero1_direita = numero1.substr(quebra_indice_fact1 * 2);
 
-    string numero2_esquerda = numero2.substr(0, quebra_indice);
-    string numero2_meio = numero2.substr(quebra_indice, quebra_indice);
-    string numero2_direita = numero2.substr(quebra_indice * 2);
+    string numero2_esquerda = numero2.substr(0, quebra_indice_fact2);
+    string numero2_meio = numero2.substr(quebra_indice_fact2, quebra_indice_fact2);
+    string numero2_direita = numero2.substr(quebra_indice_fact2 * 2);
 
-    string a0 = karatsuba(numero1_direita, numero2_direita);
+    // TODO: Revisar lógica recursão
+
+    string a0 = karatsuba(numero1_direita , numero2_direita);
     string a1 = karatsuba(concatena(numero1_direita, numero1_meio), concatena(numero2_direita, numero2_meio));
     string a2 = karatsuba(concatena(numero1_esquerda, numero1_meio), concatena(numero2_esquerda, numero2_meio));
     string a3 = karatsuba(numero1_esquerda, numero2_esquerda);
@@ -92,7 +96,7 @@ string karatsuba(string numero1, string numero2) {
     a2 = subtractStrings(a2, a3);
 
     // Shifts para as partes intermediárias
-    for (int i = 0; i < quebra_indice; ++i) {
+    for (int i = 0; i < max_len/3; ++i) {
         a1 += "0";
         a2 += "0";
     }
